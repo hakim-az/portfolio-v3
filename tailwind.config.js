fd9dcfe6c83cc2f026ac2a36055cd037d77f87b8
hakim-az <hakimazzaz93@gmail.com>
2024-06-18 15:13:05 +0100

    feat: setting eslint/prettier, installing the dependencies and adding the Inter font family

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ['Inter Regular', 'sans-serif'],
+        InterBold: ['Inter Bold', 'sans-serif'],
+        InterMedium: ['Inter Medium', 'sans-serif'],
+        InterLight: ['Inter Light', 'sans-serif'],
+        FiraCode: ['Fira Code', 'sans-serif'],
+      },
+      colors: {
+        'primaryDark': '#0A192F',
+        'primaryGreen': '#64FFDA',
+        'secondaryDark': '#112240',
+        'headingColor': '#CCD6F6',
+        'textColor': '#8892B0',
+      },
+    },
+  },
+  plugins: [],
+}
+
